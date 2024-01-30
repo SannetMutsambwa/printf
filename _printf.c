1b6297ae6a0ab4a1e131c03b2ec2b41c07ef7549
SannetMutsambwa <sannetmutsambwa@yahoo.com>
2024-01-30 03:57:31 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 /**
 *_printf - Print formatted data to stdout
 *@format: Format string
@@ -10,42 +11,62 @@
 */
 int _printf(const char *format, ...)
 {
-int char_print = 0;
 va_list args;
-va_start(args, format);
+int char_print = 0;
 if (format == NULL)
 {
 return (-1);
 }
+va_start(args, format);
+while (*format)
 {
 if (*format != '%')
 {
-printf("%s", format);
-char_print = char_print + 1;
-}
-if (*format == 'c')
-{
-char character = (char)va_arg(args, int);
-printf("%c", character);
-char_print = char_print + 1;
-}
-else if (*format == 's')
-{
-char *string = va_arg(args, char*);
-printf("%s", string);
-string = string + 1;
-char_print = char_print + 1;
-}
-if (*format == '%')
-{
-printf("%%");
+printf("%c", *format);
 char_print = char_print + 1;
 }
 else
 {
-char_print += _printf("%c", *format);
+format = format + 1;
+switch (*format)
+{
+case 'c':
+{
+char character = (char)va_arg(args, int);
+printf("%c", character);
+char_print = char_print + 1;
+break;
+}
+case 's':
+{
+char *string = va_arg(args, char *);
+if (string == NULL)
+{
+printf("(null)");
+char_print = char_print + 6;
+}
+else
+{
+printf("%s", string);
+char_print = char_print + strlen(string);
+}
+break;
+}
+case '%':
+{
+printf("%%");
+char_print = char_print + 1;
+break;
+}
+default:
+printf("%%");
+printf("%c", *format);
+char_print = char_print + 2;
+break;
+}
+}
+format = format + 1;
+}
 va_end(args);
-}
-return (0);
-}
+return (char_print);
 }
